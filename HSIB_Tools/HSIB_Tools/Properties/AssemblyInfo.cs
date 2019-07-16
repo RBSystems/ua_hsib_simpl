2f85a6a62b3981885cbe10af1523d18a413c32e8
bcampy <47375113+bcampy@users.noreply.github.com>
2019-07-15 22:43:23 -0700

    File Cleanup

--- HSIB_Tools/HSIB_Tools/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("HSIB_Tools")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HSIB_Tools")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyVersion("1.0.0.*")]
-
