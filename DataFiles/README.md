16fbde2dce11f1c2f956abb0d739f71679a631ab
bcampy <47375113+bcampy@users.noreply.github.com>
2019-07-23 18:33:39 -0700

    Deleted most files

--- DataFiles/README.md
+++
@@ -1,2 +0,0 @@
-# ua_hsib
-L3_UA_HSIB Tucson
--- SSharp/UofA_HSIB_Pro_v2.0.00/UofA_HSIB_Pro_v2.0.00/UofA_HSIB_Pro/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("UofA_HSIB_Pro")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UofA_HSIB_Pro")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyVersion("1.0.0.*")]
-
